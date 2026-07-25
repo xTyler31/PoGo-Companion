fe797889687fb808d4320001ff6d2421b1c1d44a
xTyler31 <tyler.tbone.gill31@googlemail.com>
2026-07-25 23:44:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,59 @@
+let pokemonData = [];
+
+
+fetch("data/pokemon.json")
+
+.then(response => response.json())
+
+.then(data => {
+
+    pokemonData = data;
+
+});
+
+
+
 function showType(type) {
 
-alert(type + " clicked");
+
+    let results = document.getElementById("results");
+
+
+    let matches = pokemonData.filter(pokemon =>
+        pokemon.raidType === type
+    );
+
+
+    results.innerHTML = `
+
+    <h2>${type} Attackers</h2>
+
+
+    ${matches.map(pokemon => `
+
+        <div class="card">
+
+            <h3>${pokemon.name}</h3>
+
+            <p>
+            Tier: ${pokemon.tier}
+            </p>
+
+
+            <p>
+            Best Moveset:
+            <br>
+            ${pokemon.moveset.fast}
+            +
+            ${pokemon.moveset.charged}
+            </p>
+
+        </div>
+
+
+    `).join("")}
+
+    `;
+
 
 }
